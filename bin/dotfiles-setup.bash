125d34ee3114a373e580e7e4fd25313484849275
Vincent Nagel <vnagel@umich.edu>
2019-09-23 01:15:01 -0400

    fixed dotfiles-setup backup bug
    wasn't backing up to same backup directory that was created.
    also, not sourcing bashrc anymore since using zsh.
    dotfiles-setup uses zsh instead of bash

--- bin/dotfiles-setup.bash
+++ bin/dotfiles-setup.bash
@@ -1,19 +1,19 @@
-#!/bin/bash
+#!/bin/zsh
 
 git clone --bare https://github.com/vnagel/dotfiles $HOME/.dotfiles
 function dotfiles {
    /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
 }
-mkdir .dotfiles-backup-$(date | sed "s/ /_/g")
 dotfiles checkout
 if [ $? = 0 ]; then
   echo "Checked out dotfiles";
   else
     echo "Backing up pre-existing dotfiles";
-    dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
+    backup_dir=.dotfiles-backup-$(date | sed "s/ /_/g")
+    mkdir $backup_dir
+    dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} $backup_dir/{}
 fi;
 dotfiles checkout
 dotfiles config status.showUntrackedFiles no
 source $HOME/.profile
-source $HOME/.bashrc
 source $HOME/.zshrc
